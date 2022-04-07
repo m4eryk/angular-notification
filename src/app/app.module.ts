d7b57113a938b779b999a95c9c635f27cc3b0914
AlMayorov <AlMayorov@detmir.ru>
2022-04-07 18:38:10 +0300

    add notification component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotificationComponent,
   ],
   imports: [
     BrowserModule,
